a1079adc77604d375b4c8fc3b3a4f87ca94540c9
casey-sascha-ryback <caseysascharyback@gmail.com>
2025-06-01 19:04:46 -0600

    Update app.py
    Commented off line 260 in order to test on Render web hosting

--- app.py
+++ app.py
@@ -257,5 +257,5 @@ def internal_server_error(e):
 application = app
 
 # Start the app if run directly
-if __name__ == '__main__':
+#if __name__ == '__main__':
     #app.run(host='0.0.0.0', port=5000, debug=True)
